889e09805fcb9baa3e8a7459ef45559f4c9a37be
GeorgiBisolnev <93096138+GeorgiBisolnev@users.noreply.github.com>
2022-12-16 13:16:50 +0200

    Add unit test for TestGetMostCheapThreeFlightsInMemory

---
+++ AirTicketApp.UnitTests/FlightUnitTests.cs
@@ -0,0 +1,133 @@
+﻿using AirTicketApp.Data;
+using AirTicketApp.Data.EntityModels;
+using AirTicketApp.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirTicketApp.UnitTests
+{
+    public class FlightUnitTests
+    {
+        private IRepository repo;
+        private IFlightService flightService;
+        private AirTicketAppContext DbContext;
+
+        [SetUp]
+        public void Setup()
+        {
+            var contextOptions = new DbContextOptionsBuilder<AirTicketAppContext>()
+                .UseInMemoryDatabase("FlightDB")
+                .Options;
+
+            DbContext = new AirTicketAppContext(contextOptions);
+
+            DbContext.Database.EnsureDeleted();
+            DbContext.Database.EnsureCreated();
+        }
+        [Test]
+        public async Task TestGetMostCheapThreeFlightsInMemory()
+        {
+            var repo=new Repository(DbContext);
+            flightService = new FlightService(repo);
+
+            DateTime datePast = DateTime.Parse("2022-11-19 16:30:00");
+            DateTime dateTmr = DateTime.Now.AddDays(1);
+
+            await repo.AddRangeAsync(new List<Flight>
+            {
+                new Flight()
+                    { 
+                        Id = 100,
+                        DepartureId = 1,
+                        ArrivalId = 2,
+                        ArrivalDate = dateTmr.AddHours(2),
+                        DepartureDate = dateTmr,
+                        CompanyId = 1,
+                        Price = 200,
+                        AirplaneId = 1,
+                        Snack = true,
+                        Food = true,
+                        Luggage = true,
+                        Duration = 120
+                    },
+                new Flight()
+                    {
+                        Id = 200,
+                        DepartureId = 1,
+                        ArrivalId = 2,
+                        ArrivalDate = dateTmr.AddHours(2),
+                        DepartureDate = dateTmr,
+                        CompanyId = 1,
+                        Price = 100,
+                        AirplaneId = 1,
+                        Snack = true,
+                        Food = true,
+                        Luggage = true,
+                        Duration = 120
+                    },
+                new Flight()
+                    {
+                        Id = 300,
+                        DepartureId = 1,
+                        ArrivalId = 2,
+                        ArrivalDate = dateTmr.AddHours(2),
+                        DepartureDate = dateTmr,
+                        CompanyId = 1,
+                        Price = 300,
+                        AirplaneId = 1,
+                        Snack = true,
+                        Food = true,
+                        Luggage = true,
+                        Duration = 120
+                    },
+                new Flight()
+                    {
+                        Id = 400,
+                        DepartureId = 1,
+                        ArrivalId = 2,
+                        ArrivalDate = dateTmr.AddHours(2),
+                        DepartureDate = dateTmr,
+                        CompanyId = 1,
+                        Price = 400,
+                        AirplaneId = 1,
+                        Snack = true,
+                        Food = true,
+                        Luggage = true,
+                        Duration = 120
+                    },
+                                new Flight()
+                    {
+                        Id = 401,
+                        DepartureId = 1,
+                        ArrivalId = 2,
+                        ArrivalDate = datePast.AddHours(2),
+                        DepartureDate = datePast,
+                        CompanyId = 1,
+                        Price = 5,
+                        AirplaneId = 1,
+                        Snack = true,
+                        Food = true,
+                        Luggage = true,
+                        Duration = 120
+                    },
+                });
+
+            await repo.SaveChangesAsync();
+            var flightCollection = await flightService.GetMostCheapThreeFlights();
+
+            Assert.That(3, Is.EqualTo(flightCollection.Count()));
+            Assert.That(flightCollection.Any(f=>f.Price==400), Is.False);
+            Assert.That(flightCollection.Any(f=>f.Price==5), Is.False);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            DbContext.Dispose();
+        }
+    }
+}
---
+++ AirTicketApp.UnitTests/Usings.cs
@@ -0,0 +1,4 @@
+global using NUnit.Framework;
+global using AirTicketApp.Data.Common.Repository;
+global using AirTicketApp.Services.Contracts;
+global using Moq;
